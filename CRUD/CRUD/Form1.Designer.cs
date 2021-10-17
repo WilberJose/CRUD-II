69b0f59f64a5cf2a9fcde580235fcb9b5de30063
WilberJose <wilberjose200208@gmail.com>
2021-10-17 10:13:03 -0600

    tarea

---
+++ CRUD/CRUD/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgRegistro = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.lblprecio = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btnactualizar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.grpdatos = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgRegistro)).BeginInit();
+            this.grpdatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtgRegistro
+            // 
+            this.dtgRegistro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgRegistro.Location = new System.Drawing.Point(721, 7);
+            this.dtgRegistro.Name = "dtgRegistro";
+            this.dtgRegistro.RowHeadersWidth = 51;
+            this.dtgRegistro.RowTemplate.Height = 24;
+            this.dtgRegistro.Size = new System.Drawing.Size(519, 618);
+            this.dtgRegistro.TabIndex = 0;
+            this.dtgRegistro.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgRegistro_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Compra";
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidad.Location = new System.Drawing.Point(7, 183);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(130, 32);
+            this.lblcantidad.TabIndex = 2;
+            this.lblcantidad.Text = "Cantidad";
+            // 
+            // lblprecio
+            // 
+            this.lblprecio.AutoSize = true;
+            this.lblprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprecio.Location = new System.Drawing.Point(7, 400);
+            this.lblprecio.Name = "lblprecio";
+            this.lblprecio.Size = new System.Drawing.Size(96, 32);
+            this.lblprecio.TabIndex = 3;
+            this.lblprecio.Text = "Precio";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(143, 193);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(270, 22);
+            this.txtcantidad.TabIndex = 4;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(109, 410);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(270, 22);
+            this.txtprecio.TabIndex = 5;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Location = new System.Drawing.Point(23, 503);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(104, 64);
+            this.btnguardar.TabIndex = 6;
+            this.btnguardar.Text = "Guardar";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // btnactualizar
+            // 
+            this.btnactualizar.Location = new System.Drawing.Point(212, 503);
+            this.btnactualizar.Name = "btnactualizar";
+            this.btnactualizar.Size = new System.Drawing.Size(104, 64);
+            this.btnactualizar.TabIndex = 7;
+            this.btnactualizar.Text = "Actualizar";
+            this.btnactualizar.UseVisualStyleBackColor = true;
+            this.btnactualizar.Click += new System.EventHandler(this.btnactualizar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(419, 503);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(104, 64);
+            this.btneliminar.TabIndex = 8;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // grpdatos
+            // 
+            this.grpdatos.Controls.Add(this.txtprecio);
+            this.grpdatos.Controls.Add(this.txtcantidad);
+            this.grpdatos.Controls.Add(this.lblprecio);
+            this.grpdatos.Controls.Add(this.lblcantidad);
+            this.grpdatos.Controls.Add(this.label1);
+            this.grpdatos.Location = new System.Drawing.Point(5, 6);
+            this.grpdatos.Name = "grpdatos";
+            this.grpdatos.Size = new System.Drawing.Size(576, 446);
+            this.grpdatos.TabIndex = 9;
+            this.grpdatos.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 634);
+            this.Controls.Add(this.grpdatos);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnactualizar);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.dtgRegistro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgRegistro)).EndInit();
+            this.grpdatos.ResumeLayout(false);
+            this.grpdatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgRegistro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.Label lblprecio;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btnactualizar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.GroupBox grpdatos;
+    }
+}
+
---
+++ CRUD/CRUD/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroLink1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void dtgRegistro_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Compra.getAll(dtgRegistro);
+            txtcantidad.Focus();
+
+            txtcantidad.Text = dtgRegistro.CurrentRow.Cells[0].Value.ToString();
+            txtprecio.Text = dtgRegistro.CurrentRow.Cells[1].Value.ToString();          
+
+         }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            
+            Compra.precio = txtprecio.Text;
+            Compra.insert();
+            Compra.getAll(dtgRegistro);
+        }
+
+        private void btnactualizar_Click(object sender, EventArgs e)
+        {
+            
+            Compra.precio = txtprecio.Text;
+            Compra.insert();
+            Compra.update();
+            clearData();
+            Compra.getAll(dtgRegistro);
+        }
+
+        private void clearData()
+        {
+            
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            
+            Compra.delete();
+            clearData();
+            Compra.getAll(dtgRegistro);
+        }
+    }
+ }
+            
+        
+    
